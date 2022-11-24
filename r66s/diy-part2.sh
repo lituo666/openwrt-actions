253a896f4a7860a1ddca7078ec3587709b17ef79
li tuo <lituo7516@gmail.com>
2022-11-24 02:19:44 +0000

    diy-part2脚本中luci-app-openclash更新

---
+++ r66s/diy-part2.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+git clone --depth=1 https://github.com/fw876/helloworld.git ./package/helloworld
+git clone --depth=1 https://github.com/vernesong/OpenClash.git ./package/luci-app-openclash
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome.git ./package/luci-app-adguardhome
+git clone --depth=1 https://github.com/destan19/OpenAppFilter.git ./package/OpenAppFilter
+git clone --depth=1 https://github.com/sbwml/luci-app-alist.git ./package/alist
+
+mkdir -p package/luci-app-openclash/core-lateset/tmp
+mkdir -p package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
+tar -zxf package/luci-app-openclash/core-lateset/dev/clash-linux-arm64.tar.gz -C package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core
+tar -zxf package/luci-app-openclash/core-lateset/meta/clash-linux-arm64.tar.gz -C package/luci-app-openclash/core-lateset/tmp
+mv package/luci-app-openclash/core-lateset/tmp/* package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/clash_meta
+cp package/luci-app-openclash/core-lateset/premium/clash-linux-arm64-*.gz package/luci-app-openclash/core-lateset/tmp
+gzip -d package/luci-app-openclash/core-lateset/tmp/*
+chmod +x package/luci-app-openclash/core-lateset/tmp/*
+mv package/luci-app-openclash/core-lateset/tmp/* package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/clash_tun
+echo "==========openclash的内核目录core如下=========="
+ls -al package/luci-app-openclash/luci-app-openclash/root/etc/openclash/core/
+echo "============================================"
+
+echo "diy-part2.sh运行完成"
